26ab2b75d111dadd7244d869973419992bf017d8
dgarciaa2024 <dgarciaa@unah.hn>
2025-03-23 18:48:13 -0600

    vista correo duracion contraseña

--- resources/views/emails/ResetPassword.blade.php
+++ resources/views/emails/ResetPassword.blade.php
@@ -1,3 +1,3 @@
 <p>Hola, has solicitado el restablecimiento de la contraseña de tu cuenta.</p>
 <p>Tu nueva contraseña temporal es: {{ $newPassword }}</p>
-<p>Esta contraseña expirará en {{ $expirationTime }} horas. Si no solicitaste este cambio, ignora este correo.</p>
+<p>Esta contraseña expirará en {{ $expirationTime }} día(s). Si no solicitaste este cambio, ignora este correo.</p>
